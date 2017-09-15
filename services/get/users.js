2abce380560ab373d0056e453c2846166d18213f
jerry <445242970@qq.com>
2017-09-15 18:06:03 +0800

    first commit

--- services/get/users.js
+++ services/get/users.js
@@ -1,18 +1,29 @@
-var g = require('dyson-generators');
+var random = require('mockjs').Random;
 
 var user = {
     path: '/user',
     template: {
-        id: g.id,
-        user: g.name,
-        city: g.address.city
-    }
+        id: function() {
+            return random.id()
+        },
+        name: function() {
+            return random.cname()
+        },
+        avatar: function() {
+            return random.image()
+        },
+        city: function() {
+            return random.city()
+        },
+    },
+    cache: false
 };
 
 var users = {
     path: '/users',
     collection: true,
-    template: user.template
+    template: user.template,
+    size: 5
 };
 
 module.exports = [user, users];
